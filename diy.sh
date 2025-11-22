aeea52d461d30f9bcb7e3f7c1bbeaf14bfee601e
dongjany <3145255636@qq.com>
2025-11-22 21:00:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,4 +3,4 @@ sed -i 's/192.168.1.1/192.168.2.15/g' package/base-files/files/bin/config_genera
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 # Modify default name
-sed -i 's/OpenWrt/OneCloud/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OneCloud/g' package/base-files/files/bin/config_generate
