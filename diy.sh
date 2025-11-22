a3fb135c692bf6f171372c3c548ef76a31c9a339
dongjany <3145255636@qq.com>
2025-11-22 21:00:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
 # Modify default ip
-sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.15/g' package/base-files/files/bin/config_generate
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 # Modify default name
