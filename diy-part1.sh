e7c318bb8abd28eb39aa9da1f063f0d02662292e
dongjany <3145255636@qq.com>
2025-11-22 21:38:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,4 @@
+# Add a feed source
 sed -i '/^#/d' feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
